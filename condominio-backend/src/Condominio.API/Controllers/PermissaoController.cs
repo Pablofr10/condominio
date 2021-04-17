063cfad25997e805d8d3680867035c4a208c1a15
Pablo Araújo <rafanet.live@gmail.com>
2021-04-17 20:32:31 -0300

    Iniciando recuperar senha

--- condominio-backend/src/Condominio.API/Controllers/PermissaoController.cs
+++ condominio-backend/src/Condominio.API/Controllers/PermissaoController.cs
@@ -1,13 +1,12 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Condominio.Domain.Dtos;
 using Condominio.Domain.Dtos.Identity;
 using Condominio.Domain.Dtos.Request;
-using Condominio.Domain.Dtos.Response;
 using Condominio.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +15,7 @@ namespace Condominio.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes=JwtBearerDefaults.AuthenticationScheme, Roles = "Administrador")]
     public class PermissaoController : BaseController
     {
         private readonly RoleManager<Role> _roleManager;
@@ -25,73 +25,54 @@ namespace Condominio.API.Controllers
             _roleManager = roleManager;
         }
 
-        [AllowAnonymous]
         [HttpGet]
         public IActionResult GetPermissoes()
         {
             var permicoes = GetService<IPermissaoService>().GetPermissoes();
 
-            if (permicoes.Any())
-            {
-                return Ok(permicoes);
-            }
+            if (permicoes.Any()) return Ok(permicoes);
 
             return NoContent();
         }
         
-        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPermissao(int id)
         {
             var permissao = await GetService<IPermissaoService>().GetPermissao(id);
 
-            if (permissao.Any())
-            {
-                return Ok(permissao);
-            }
+            if (permissao.Any()) return Ok(permissao);
 
             return NoContent();
         }
 
-        [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> CriaPermissao(PermissoesRequest role)
         {
             var isPermissaoCriada = await GetService<IPermissaoService>().CriarPermissao(role);
 
-            if (isPermissaoCriada)
-            {
-                return Ok("Permissao criada com sucesso");
-            }
+            if (isPermissaoCriada) return Ok("Permissao criada com sucesso");
 
             return BadRequest("Erro ao criar a permissão");
         }
         
-        [AllowAnonymous]
         [HttpPut]
         public async Task<IActionResult> CriaPermissao(PermissoesEditarRequest role)
         {
             var isPermissaoEditada = await GetService<IPermissaoService>().EditarPermissoes(role);
 
-            if (isPermissaoEditada)
-            {
-                return Ok("Permissao editada com sucesso");
-            }
+            if (isPermissaoEditada) return Ok("Permissao editada com sucesso");
 
             return BadRequest("Erro ao editar a permissão");
         }
         
-        [Route("EditarPermissoes")]
-        [AllowAnonymous]
         [HttpPost]
+        [Route("EditarPermissoes")]
+
         public async Task<IActionResult> EditarPermissoesUsuarios(List<UserRoleDto> role, string roleId)
         {
             var isPermissaoEditada = await GetService<IPermissaoService>().EditarPermissoesUsuarios(role, roleId);
 
-            if (isPermissaoEditada)
-            {
-                return Ok("Permissões editada com sucesso");
-            }
+            if (isPermissaoEditada) return Ok("Permissões editada com sucesso");
 
             return BadRequest("Erro ao editar a permissão");
         }
